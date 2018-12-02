7f09ea64a6619295058dae36d716e13dae572cb2
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-03 00:21:55 +0530

    realtor admin view modification

--- realEstate_project/realtors/admin.py
+++ realEstate_project/realtors/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('name','is_mvp','email','hire_date')
+    list_display_links = ('name','email')
+    list_per_page = 10
+    search_fields = ('name',)
+    list_editable = ('is_mvp',)
+
+admin.site.register(Realtor,RealtorAdmin)
