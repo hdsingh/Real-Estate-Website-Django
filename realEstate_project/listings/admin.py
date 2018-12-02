3904068695ffb1af8554ea1de21afbf984a3ec1e
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-03 00:14:50 +0530

    listing admin view customised

--- realEstate_project/listings/admin.py
+++ realEstate_project/listings/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'is_published',
+                    'price','list_date','realtor')
+    list_display_links = ('id','title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title','description','city','state')
+    list_per_page = 10
+
+admin.site.register(Listing,ListingAdmin)
