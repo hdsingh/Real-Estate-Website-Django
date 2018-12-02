52f2f3c8dde9819a6f9dfd1f34416285abb8f9b8
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-02 20:35:32 +0530

    postgres settings

--- realEstate_project/realEstate/settings.py
+++ realEstate_project/realEstate/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'realEstate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'redb',
+        'USER': 'postgres',
+        'PASSWORD':'admin',
+        'HOST': 'localhost'
     }
 }
 
