e27a914ad6a2c98807f7371f158b07930117f3fb
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-05 16:34:33 +0530

    single listing page

--- realEstate_project/listings/views.py
+++ realEstate_project/listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from django.core.paginator import Paginator
 
 from .models import Listing
@@ -15,8 +15,12 @@ def index(request):
     return render(request,'listings/listings.html',
     context)
 
-def listing(request,listing_id):
-    return render(request,'listings/listing.html')
+def listing(request,listing_id): 
+    listing = get_object_or_404(Listing, pk = listing_id)
+    context = {
+        'listing':listing
+    }
+    return render(request,'listings/listing.html',context)
 
 def search(request):
     return render(request,'listings/search.html')
