ab6f98659d8244620c48c8130f3722a493fbad84
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-05 17:53:18 +0530

    search functionality added

--- realEstate_project/listings/views.py
+++ realEstate_project/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render ,get_object_or_404
 from django.core.paginator import Paginator
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -23,4 +24,46 @@ def listing(request,listing_id):
     return render(request,'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords 
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains = keywords)
+    
+    # city 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact = city)
+    
+    # state 
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact = state)
+
+    # bedrooms 
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte = bedrooms)
+    
+    # price 
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte = price)
+
+
+
+
+
+    context =  {
+        "price_choices":price_choices,
+        "state_choices":state_choices,
+        "bedroom_choices":bedroom_choices,
+        "listings":queryset_list
+    }
+    return render(request,'listings/search.html',context)
