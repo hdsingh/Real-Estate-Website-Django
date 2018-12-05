8c29b6a8f11f3f1d0490fbcc261e2cbfb4fbc767
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-05 11:42:07 +0530

    display listing in templates

--- realEstate_project/listings/views.py
+++ realEstate_project/listings/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Listing
 
 def index(request):
-    return render(request,'listings/listings.html')
+    listings = Listing.objects.all() 
 
-def listing(request):
+    context = {
+        'listings':listings}
+         
+    return render(request,'listings/listings.html',
+    context)
+
+def listing(request,listing_id):
     return render(request,'listings/listing.html')
 
 def search(request):
