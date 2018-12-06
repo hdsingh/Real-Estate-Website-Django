930a7ee1b5d98f8f9ed780f492de52c8cf9eee72
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-06 23:47:26 +0530

    user Registeration

--- realEstate_project/accounts/views.py
+++ realEstate_project/accounts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
 
 def login(request):
-    if request.method == "POST":
-        return 
+    if request.method == 'POST':
+        messages.error(request,'Testing Error Message')
+        return redirect('login')
     else:
         return render(request,'accounts/login.html')
 
@@ -14,8 +17,42 @@ def logout(request):
     return redirect("index")
 
 def register(request):
-    if request.method == 'POST':
-        return redirect('index')
+    if request.method == "POST":
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username = username).exists():
+                messages.error(request,"That username is taken.")
+                return redirect('register')
+            else:
+                if User.objects.filter(email = email).exists():
+                    messages.error(request,"That email is taken.")
+                    return redirect('register')
+                else:
+                    # looks good
+                    user = User.objects.create_user(username = username,
+                    password = password,email=email,first_name = first_name,
+                    last_name = last_name)
+                    user.save()
+                    # Login after register
+                    auth.login(request,user)
+                    messages.success(request,"You are now logged in.")
+                    return redirect('index')
+
+                    # # Login manually 
+                    # messages.success(request,"You can now log in.")
+                    # return redirect('login')
+        else:
+            messages.error(request,'Passwords do not match.')
+            return redirect('register')
     else:
         return render(request,'accounts/register.html')
 
--- realEstate_project/realEstate/settings.py
+++ realEstate_project/realEstate/settings.py
@@ -133,3 +133,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR : 'danger'
+}
