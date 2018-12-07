02301b3a8ef50bdc78e03ec2f2d7b986851a1f4a
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-07 10:51:59 +0530

    contact form update

--- realEstate_project/realEstate/urls.py
+++ realEstate_project/realEstate/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path('contacts/',include('contacts.urls')),
     path('listing/',include('listings.urls')),
     path('user/',include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
