3a0a112ae3039c0c3c3cb0d459a53385aaafb716
hdsingh <harmandeepsinghhds@gmail.com>
2018-12-07 11:24:19 +0530

    contact form submission

--- realEstate_project/contacts/models.py
+++ realEstate_project/contacts/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
     email = models.CharField(max_length = 100)
     phone = models.CharField(max_length = 20)
     message = models.TextField(blank=True)
-    contact_date = models.DateTimeField(datetime.now,blank= True)
+    contact_date = models.DateTimeField(default = datetime.now)
     user_id = models.IntegerField(blank = True)
     def __str__(self):
         return self.name
--- realEstate_project/contacts/views.py
+++ realEstate_project/contacts/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def contact(request):
-    return
+    if request.method == "POST":
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing_id = listing_id,
+                            listing = listing,
+                            name = name,
+                            email = email,
+                            phone = phone,
+                            message = message,
+                            user_id =user_id,)
+        contact.save()
+        
+        messages.success(request,"Your request has been submitted. "+
+        "We will get backto you soon.")
+        
+        return redirect('/listing/'+listing_id)
